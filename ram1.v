9e7148f17591dcaaf081695e92ee60f024a00fc2
李岩松“ <2016011268@secoder.net>
2018-12-04 23:50:59 +0800

    add s_port and new ram

--- ram1.v
+++ ram1.v
@@ -19,33 +19,119 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module ram1(
-		input [17:0] addr,
-		input [15:0] data,
-		output [17:0] Ram1Addr,
-		inout wire [15:0] Ram1Data,
-		output Ram1OE,
-		output Ram1WE,
-		output [15:0] mem1res_o,
-		input read,	//0-read 1-write
+		input data_ready_i,
+		input tbre_i,
+		input tsre_i,
+		output wrn_o,
+		output rdn_o,
+		output [17:0] Ram1Addr_o,
+		inout [15:0] Ram1Data_io,
+		output Ram1OE_o,
+		output Ram1WE_o,
+		output Ram1EN_o,
+		
+		input is_RAM1_i,
+		input is_UART_i,
+		input [17:0] addr_i,      //17:0 !!!!!!!!!!!!
+		input [15:0] data_i,
+		
+		input isread_i,
+		input iswrite_i,
+		output [15:0] ram1res_o,
 		input clk
     );
-reg [15:0] memres;
-assign mem1res_o = memres;
+	 
+reg is_uart_read;			
+reg is_uart_write;
+reg is_ram_read;			
+
+wire read; 					
+assign read = is_ram_read | is_uart_read; 				
+
+assign rdn_o = !is_uart_read ? 1'b1 : !clk;
+assign wrn_o = !is_uart_write ? 1'b1 : !clk;
+
 wire oe;
 wire we;
+reg en;
+assign Ram1OE_o = !is_ram_read ? 1'b1 : !clk;
+assign Ram1WE_o = !is_ram_read ? !clk : 1'b1;
+assign Ram1EN_o = en;
 
-assign Ram1OE = oe;
-assign Ram1WE = we;
+assign Ram1Data_io = !read ? data_i : 16'bz;
+assign Ram1Addr_o = addr_i;
 
-assign Ram1Data = !read ? 16'bz : data;
-assign Ram1Addr = addr;
+reg is_check;
 
-assign oe = !read ? !clk : 1'b1;
-assign we = !read ? 1'b1 : !clk;
-
+reg [15:0] mem1res;
+reg [15:0] uart_check;
+assign ram1res_o = is_check ? uart_check : mem1res;
+/*
+assign oe = !is_ram_read ? 1'b1 : !clk;
+assign we = !is_ram_read ? !clk : 1'b1;
+*/
+always @(*) begin
+	case(is_UART_i)
+		1'b0:begin
+			is_uart_read <= 1'b0;
+			is_uart_write <= 1'b0;
+			is_check <= 1'b0;
+		end
+		1'b1:begin
+			case(addr_i[15:0])
+				16'hbf01:begin
+					is_check <= 1'b1;
+					is_uart_read <= 1'b0;
+					is_uart_write <= 1'b0;
+					uart_check [15:0] <= {14'b10101000000000,data_ready_i,(tbre_i & tsre_i)};
+				end
+				16'hbf00:begin
+					is_check <= 1'b0;
+					case({isread_i,iswrite_i})
+						2'b01:begin							//write
+							is_uart_read <= 1'b0;
+							is_uart_write <= 1'b1;
+						end
+						2'b10:begin							//read
+							is_uart_read <= 1'b1;
+							is_uart_write <= 1'b0;
+						end
+						default:begin
+							is_uart_read <= 1'b0;
+							is_uart_write <= 1'b0;
+						end
+					endcase
+				end
+			endcase
+		end
+	endcase
+	
+	case(is_RAM1_i)
+		1'b0:begin
+			en <= 1'b1;
+		end
+		1'b1:begin
+			case({isread_i,iswrite_i})
+				2'b01:begin							//write
+					en <= 1'b0;
+					is_ram_read <= 1'b0;
+				end
+				2'b10:begin							//read
+					en <= 1'b0;
+					is_ram_read <= 1'b1;
+				end
+				default:begin
+					en <= 1'b1;
+					is_ram_read <= 1'b1;
+				
+				end
+			endcase
+		end
+	endcase
+end
 always@(negedge clk) begin
-	if (read == 1'b0) begin
-		memres <= Ram1Data;
+	if (isread_i == 1'b1) begin
+		mem1res <= Ram1Data_io;
 	end
 end
 endmodule
--- ram1_test.v
+++ ram1_test.v
@@ -4,7 +4,7 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   22:03:51 12/03/2018
+// Create Date:   23:22:32 12/04/2018
 // Design Name:   ram1
 // Module Name:   G:/ISE_Project/zzcpu/ram1_test.v
 // Project Name:  CPU
@@ -25,57 +25,76 @@
 module ram1_test;
 
 	// Inputs
-	reg [17:0] addr;
-	reg [15:0] data;
-	reg read;
+	reg data_ready_i;
+	reg tbre_i;
+	reg tsre_i;
+	reg is_RAM1_i;
+	reg is_UART_i;
+	reg [17:0] addr_i;
+	reg [15:0] data_i;
+	reg isread_i;
+	reg iswrite_i;
 	reg clk;
 
 	// Outputs
-	wire [17:0] Ram1Addr;
-	wire Ram1OE;
-	wire Ram1WE;
+	wire wrn_o;
+	wire rdn_o;
+	wire [17:0] Ram1Addr_o;
+	wire Ram1OE_o;
+	wire Ram1WE_o;
+	wire Ram1EN_o;
+	wire [15:0] ram1res_o;
 
 	// Bidirs
-	wire [15:0] Ram1Data;
+	wire [15:0] Ram1Data_io;
 
 	// Instantiate the Unit Under Test (UUT)
 	ram1 uut (
-		.addr(addr), 
-		.data(data), 
-		.Ram1Addr(Ram1Addr), 
-		.Ram1Data(Ram1Data), 
-		.Ram1OE(Ram1OE), 
-		.Ram1WE(Ram1WE), 
-		.read(read), 
+		.data_ready_i(data_ready_i), 
+		.tbre_i(tbre_i), 
+		.tsre_i(tsre_i), 
+		.wrn_o(wrn_o), 
+		.rdn_o(rdn_o), 
+		.Ram1Addr_o(Ram1Addr_o), 
+		.Ram1Data_io(Ram1Data_io), 
+		.Ram1OE_o(Ram1OE_o), 
+		.Ram1WE_o(Ram1WE_o), 
+		.Ram1EN_o(Ram1EN_o), 
+		.is_RAM1_i(is_RAM1_i), 
+		.is_UART_i(is_UART_i), 
+		.addr_i(addr_i), 
+		.data_i(data_i), 
+		.isread_i(isread_i), 
+		.iswrite_i(iswrite_i), 
+		.ram1res_o(ram1res_o), 
 		.clk(clk)
 	);
 
 	initial begin
 		// Initialize Inputs
-		addr = 0;
-		data = 0;
-		read = 0;
+		data_ready_i = 0;
+		tbre_i = 0;
+		tsre_i = 0;
+		is_RAM1_i = 0;
+		is_UART_i = 0;
+		addr_i = 0;
+		data_i = 0;
+		isread_i = 0;
+		iswrite_i = 0;
 		clk = 0;
-		
+
 		// Wait 100 ns for global reset to finish
 		#100;
-       clk = 1'b1;
-		 addr = 18'b000000000000000001;
-		 data = 16'h0004;
-		 read = 1'b1;
-		 #10
-		 clk = 1'b0;
-		 
-		 #10
-		 clk = 1'b1;
-		 addr = 18'b000000000000000001;
-		 read = 1'b0;
-		 
-		 #10
-		 clk = 1'b0;
-		 
+        
 		// Add stimulus here
-
+		addr_i = {2'b00,16'hbf01};
+		data_i = 16'h0004;
+		is_UART_i = 1'b1;
+		isread_i = 1'b1;
+		#20
+		clk = 1'b1;
+		#20
+		clk = 1'b0;
 	end
       
 endmodule
